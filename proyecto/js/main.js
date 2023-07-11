df5a3469885ac291bb18dc3b13fec4bae54b304a
mauroacampani <mauroacampani@gmail.com>
2023-07-10 23:23:33 -0300

    cambio titulo

--- proyecto/js/main.js
+++ proyecto/js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-primary">
 <div class="container">
-<h6><a class="navbar-brand" href="../templates/prueba.html">Peliculas</a></h6>
+<h6><a class="navbar-brand" href="../templates/prueba.html">Películas</a></h6>
 
 </div>
 </nav>`
